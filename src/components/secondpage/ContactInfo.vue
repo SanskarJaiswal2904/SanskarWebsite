9195fa4e55836a699b2080b07cc3fc1a2b0e7fd8
SanskarJaiswal2904 <sanskarjaiswal2904@gmail.com>
2025-01-15 15:00:14 +0530

    fixed design bug

--- src/components/secondpage/ContactInfo.vue
+++ src/components/secondpage/ContactInfo.vue
@@ -20,7 +20,6 @@
                 </a>
             </div>
         </div>
-        <img id="gif-image" src="../../assets/flowers4-unscreen.gif" style="display: block; margin: auto; height: 520px; margin-top: 40px">
     </div>
 
   
